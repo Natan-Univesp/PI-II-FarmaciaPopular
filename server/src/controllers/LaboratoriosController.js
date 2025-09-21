52bf8132ddd4c3249ad3ce39c2e88e68d2ca6f27
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2025-09-20 21:02:23 -0300

    feat(server/Laboratorios): adiciona controller e router de Laboratorio

---
+++ server/src/controllers/LaboratoriosController.js
@@ -0,0 +1,133 @@
+const CannotCreateError = require("../classes/CannotCreateError.js");
+const FieldUndefinedError = require("../classes/FieldUndefinedError.js");
+const NotFoundError = require("../classes/NotFoundError.js");
+const errorResponse = require("../helper/ErrorResponseHelper.js");
+
+async function getAllLaboratorios(req, res) {
+   try {
+      const allLaboratorios = "service aqui";
+      return res.status(200).json(allLaboratorios);
+
+   } catch (error) {
+      errorResponse(error, res);
+   }
+}
+
+async function getLaboratorioById(req, res) {
+   try {
+      const id = Number(req.params.id);
+
+      if(!id) {
+         throw new FieldUndefinedError("Campo ID não identificado", {
+            fields: {
+               id
+            }
+         })
+      };
+
+      const laboratorio = "service aqui";
+
+      if(!laboratorio) {
+         throw new NotFoundError("Laboratório não encontrado", {
+            fields: {
+               id
+            }
+         })
+      }
+
+      return res.status(200).json(laboratorio)
+
+   } catch (error) {
+      errorResponse(error, res);
+   }
+}
+
+async function getAllLaboratoriosForSelect(req, res) {
+   try {
+      const allLaboratorios = "service aqui";
+      return res.status(200).json(allLaboratorios);
+   } catch (error) {
+      errorResponse(error, res);
+   }
+}
+
+async function createLaboratorio(req, res) {
+   try {
+      const {
+         nome_laboratorio,
+         cnpj, 
+         endereco
+      } = req.body;
+
+      if(!nome_laboratorio || !cnpj || !endereco) {
+         throw new FieldUndefinedError("Nenhum campo identificado", {
+            dados_passados: {
+               nome_laboratorio,
+               cnpj,
+               endereco
+            }
+         });
+      }
+
+      const createdLaboratorio = "service aqui";
+
+      if(!createdLaboratorio) {
+         throw new CannotCreateError("Erro ao cadastrar Laboratório", {
+            laboratorioData: req.body,
+            inCreated: createdLaboratorio
+         })
+      }
+
+      return res.status(201).json({
+         status: "success",
+         message: "Laboratório cadastrado com sucesso!",
+         data: createdLaboratorio
+      })
+
+   } catch (error) {
+      errorResponse(error, res);
+   }
+}
+
+async function updateLaboratorio(req, res) {
+   try {
+      const id = Number(req.params.id);
+      const {
+         nome_laboratorio,
+         cnpj, 
+         endereco
+      } = req.body;
+
+      if(!id || (nome_laboratorio && cnpj && endereco)) {
+         throw new FieldUndefinedError("Nenhum campo identificado", {
+            fields: {
+               id,
+               nome_laboratorio,
+               cnpj,
+               endereco
+            }
+         })
+      }
+
+      const [rowAffected] = "service aqui";
+
+      if(rowAffected > 0) {
+         return res.status(200).json({
+            status: "success",
+            message: "Informações de Laboratório alterado com sucesso!",
+         });
+      }
+
+
+   } catch (error) {
+      errorResponse(error, res);
+   }
+}
+
+module.exports = {
+   getAllLaboratorios,
+   getAllLaboratoriosForSelect,
+   getLaboratorioById,
+   createLaboratorio,
+   updateLaboratorio
+}
---
+++ server/src/routes/LaboratoriosRouter.js
@@ -0,0 +1,18 @@
+const Router = require("express");
+const laboratoriosController = require("../controllers/LaboratoriosController.js");
+
+const router = Router();
+
+router
+   .route("/")
+   .get(laboratoriosController.getAllLaboratorios)
+   .post(laboratoriosController.createLaboratorio)
+
+router
+   .route("/select-options")
+   .get(laboratoriosController.getAllLaboratoriosForSelect)
+
+router
+   .route("/:id")
+   .get(laboratoriosController.getLaboratorioById)
+   .post(laboratoriosController.updateLaboratorio)
