9d7df0133c47b443b5b1479eaac2287399f7c03a
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2025-10-26 23:23:51 -0300

    feat(client/ValidateRoutes): adiciona rotas de validação

---
+++ client/src/components/RoutesValidate/ValidateAdmin.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useUser } from "../../context/UserContext";
+
+export function ValidateAdmin({children}) {
+   const {user} = useUser();
+
+   if(user?.nivel_acesso > 1) {
+      return <Navigate to={"/"}/>
+   }
+
+   return children;
+}
