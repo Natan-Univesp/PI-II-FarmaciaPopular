ac48a6e2de311a83d438cb9357a5fda5d1b61392
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2025-11-08 19:25:01 -0300

    fix(client): corrige case-sensitive de caminhos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import "./App.css";
-import { Navbar } from "./components/NavBar/NavBar";
-import { Topbar } from "./components/TopBar/TopBar";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Topbar } from "./components/Topbar/Topbar";
 import { useNavbar } from "./context/NavbarContext";
 import { ValidateLogin } from "./components/RoutesValidate/ValidateLogin";
 import { AcessibilityButton } from "./components/Acessibility/AcessibilityButton";
