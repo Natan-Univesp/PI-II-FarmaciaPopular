5d596b7bcbc2919f29b76eb91020b146ed65235a
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2025-11-08 20:59:51 -0300

    feat(client): implementa otimização de client

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+   plugins: [react()],
+   build: {
+      // Otimizações para reduzir memória
+      minify: "esbuild",
+      target: "esnext",
+
+      // Divide os chunks para evitar arquivos muito grandes
+      rollupOptions: {
+         output: {
+            manualChunks: (id) => {
+               if (id.includes("node_modules")) {
+                  if (id.includes("react")) return "vendor-react";
+                  if (id.includes("react-icons")) return "vendor-icons";
+                  return "vendor";
+               }
+            },
+         },
+      },
+   },
+});
