1a4cda4df652d497b467980696530700280cd6f1
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2025-11-08 23:56:55 -0300

    retorna o client em seu estado original

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,33 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-   plugins: [react()],
-   build: {
-      // Otimizações para reduzir memória
-      minify: "esbuild",
-      target: "esnext",
-
-      // Divide os chunks para evitar arquivos muito grandes
-      rollupOptions: {
-         output: {
-            manualChunks: (id) => {
-               if (id.includes("node_modules")) {
-                  if (id.includes("react")) return "vendor-react";
-                  if (id.includes("react-icons")) return "vendor-icons";
-                  return "vendor";
-               }
-            },
-         },
-      },
-   },
-   preview: {
-      host: true,
-      allowedHosts: [
-         "farmaciapopular-production.up.railway.app",
-         ".railway.app"
-      ]
-
-   }
-});
+  plugins: [react()],
+})
