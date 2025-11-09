04d6bad6c0d41298c547613936d2f4fb44ff6f59
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2025-11-09 01:19:35 -0300

    feat(server): adiciona configuração do CORS

--- server/index.js
+++ server/index.js
@@ -12,7 +12,12 @@ const PORT = 3000;
 const HOST_IP = process.env.IP_FIXED || "0.0.0.0";
 
 dbConnectionTest();
-app.use(cors());
+app.use(cors({
+   origin: process.env.CLIENT_URL || "http://localhost:5173",
+   credentials: true,
+   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+   allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.static("public"));
 app.use(Routes);
