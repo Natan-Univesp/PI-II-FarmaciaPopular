21b2622f550ead643a432d134ba87c9b2964d4ea
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2025-11-09 03:03:03 -0300

    adiciona correções

--- server/index.js
+++ server/index.js
@@ -7,55 +7,16 @@ const Routes = require("./src/routes/index.js");
 
 dotenv.config();
 
-const allowedOrigins = [
-   "http://localhost:5173",
-   "https://pi-farmacia-popular.netlify.app"
-]
-
-const corsOptions = {
-   origin: (origin, callback) => {
-
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-         callback(null, true); // Origin is allowed
-      } else {
-         callback(new Error("Not allowed by CORS")); // Origin is not allowed
-      }
-   },
-   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow specific methods
-   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"], // Specify allowed headers
-   credentials: true, // Include credentials (optional)
-   optionsSuccessStatus: 204, // For legacy browser support
-};
-
 const app = express();
 const PORT = 3000;
-const HOST_IP = process.env.IP_FIXED || "0.0.0.0";
 
 dbConnectionTest();
 
-app.use((req, res, next) => {
-   console.log('=== REQUEST INFO ===');
-   console.log('Method:', req.method);
-   console.log('Path:', req.path);
-   console.log('Origin:', req.headers.origin);
-   console.log('Headers:', req.headers);
-   next();
-});
-
-app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-   if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Origin', req.headers.origin);
-      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, PATCH');
-      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-      res.header('Access-Control-Allow-Credentials', 'true');
-      return res.status(200).end();
-   }
-   next();
-});
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 app.use(Routes);
-app.listen(PORT, HOST_IP, () => console.log("rodando"));
+app.use("/", (req, res) => {
+   res.send("OK")
+})
+app.listen(process.env.PORT || PORT, () => console.log("rodando"));
